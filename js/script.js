15837f2d25567a8a891d9f8fd857447121039c9c
unknown <sayttplx@gmail.com>
2020-12-08 11:33:27 +0100

    trying out some svgs, added a bit css

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let button = document.getElementById("button");
 let smallSize = document.getElementById('small-size');
 let mediumSize = document.getElementById('medium-size');
 let largeSize = document.getElementById('large-size');
-let size = '_s';
+let size = '_q';
 
 
 let getData = async (query) => {
@@ -11,8 +11,8 @@ let getData = async (query) => {
     const KEY = "19d3e6e0acfe9c438f368e2c2bab1c5d";
     const METHOD = "flickr.photos.search";
     const FORMAT = "json&nojsoncallback=1";
-    const PAGES_12 = "per_page=12";
-    //const PAGES_24 = "per_page=24";
+    const PAGES_12 = "per_page=64";
+    // const PAGES_24 = "per_page=24";
     // const PAGES_36 = "per_page=36";
 
     let url = `${API}?api_key=${KEY}&method=${METHOD}&${PAGES_12}&sort=relevance&format=${FORMAT}&text=${query}`;
@@ -31,14 +31,29 @@ function getImage(data) {
         
     data.photos.photo.forEach((element) => {
         let imageItem = document.createElement("li");
-        imageItem.classList.add('test')
+        imageItem.classList.add('photo-style')
         imageItem.innerHTML = `<img src="https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}${size}.jpg"/>`;
         render.appendChild(imageItem);
     });
 };
 
-const selectElement = document.querySelector('.size');
-    selectElement.addEventListener('change', (event) => {
-    size = event.target.value;
+// const selectElement = document.querySelector('.size');
+//     selectElement.addEventListener('click', (event) => {
+//     size = event.target.value;
+//     console.log(size)
+// });
+
+smallSize.addEventListener('click', () => {
+    size = '_s'
+    console.log(size)
 });
 
+mediumSize.addEventListener('click', () => {
+    size = '_q'
+    console.log(size)
+});
+
+largeSize.addEventListener('click', () => {
+    size = '_b'
+    console.log(size)
+});
